953fb5965675ca6c7f86044543d1072ae845e122
ljflint <79536506+LjFlint@users.noreply.github.com>
2021-06-15 22:41:42 -0400

    a lot of the mechanism function added

--- interrogate_spf.py
+++ interrogate_spf.py
@@ -8,7 +8,7 @@ from typing import Tuple
 
 resolver = dns.resolver
 
-def extract_qualifier(f: str) -> Tuple[str,str] : 
+def extract_qualifier(f: str) -> Tuple[str,str]: 
     if f.startswith('-'):
         return "Fail", f[1:]
     elif f.startswith('~'):
@@ -24,17 +24,23 @@ def process_mech_all(m: dict, domain: str) -> dict:
     return {"value": "all"}
 
 def process_mech_ip4(m: dict, domain: str) -> dict:
-    net = netaddr.IPNetwork(m["value"])
+    try:
+        net = netaddr.IPNetwork(m["value"])
+    except netaddr.core.AddrFormatError:
+        return {"error": f"Malformed Network Address: {m['value']}"}
     return {"network_size": net.size, "network": str(net)}
 
 def process_mech_ip6(m: dict, domain: str) -> dict:
-    net = netaddr.IPNetwork(m["value"])
-    return{"network_size": net.size,"network": str(net)}
+    try:
+        net = netaddr.IPNetwork(m["value"])
+    except netaddr.core.AddrFormatError:
+        return {"error": f"Malformed Network Address: {m['value']}"}
+    return {"network_size": net.size,"network": str(net)}
 
-def process_mech_a(m: dict, domain: str) -> list:
+def process_mech_a(m: dict, domain: str) -> dict:
     value = m["value"] or domain
     prefix = m["prefix"] or ""
-    return query_a(value)
+    return {"records": query_a(value)}
 
 def process_mech_mx(m: dict, domain: str) -> dict:
     value = m["value"] or domain
@@ -55,7 +61,10 @@ def process_mech_exp(m: dict, domain: str) -> dict:
     return m
 
 def process_mech_redirect(m: dict, domain: str) -> dict:
-    return m
+    return process_domain(m["value"])
+
+def process_unknown_mechanism(m: dict, domain: str) -> dict:
+    return {"error": f"Unknown mechanism: {m['mechanism']}"}
 
 mechanism_map = {
      "all": process_mech_all, 
@@ -93,7 +102,7 @@ def read_file(filename: str) -> list:
 def query(domain: str, record: str):
     try:
         return resolver.resolve(domain,record)
-    except (dns.resolver.NoAnswer, dns.resolver.NoNameservers):
+    except (dns.resolver.NoAnswer, dns.resolver.NoNameservers,dns.resolver.NXDOMAIN):
         return None
 
 
@@ -103,7 +112,7 @@ def query_spf(domain: str) -> str:
         return ""
     for txtd in q.rrset:
         if txtd.strings[0].decode('utf-8').split()[0] == 'v=spf1':
-            return txtd.strings[0].decode('utf-8')
+            return txtd.strings[0].decode('utf-8').lower()
 
     return ""
 
@@ -112,7 +121,7 @@ def query_mx(domain: str) -> dict:
     q = query(domain,'mx')
     if not q:
         return {}
-    return {mx.exchange: {
+    return {str(mx.exchange): {
             "preference": mx.preference, 
             "records": query_a(mx.exchange), 
         } for mx in q.rrset}
@@ -122,7 +131,7 @@ def query_a(domain: str) -> list:
     q = query(domain,'a')
     if not q:
         return []
-    return [netaddr.IPAddress(record.address) for record in q.rrset]
+    return [str(netaddr.IPAddress(record.address)) for record in q.rrset]
 
 
 def query_ptr(ipaddr: str) -> str:
@@ -141,6 +150,10 @@ def parse_spf(spf: str):
     results = []
     for f in fields[1:]:
         r = {"original": f}
+        if '=' in f:
+            r["mechanism"] = f.split('=')[0]
+            r["value"] = f.split('=')[1]
+            continue
         r["qualifier"], f = extract_qualifier(f)
         r["mechanism"] = f.split('/')[0].split(':')[0]
         r["prefix"] = f.split('/')[1] if '/' in f else ""
@@ -148,6 +161,18 @@ def parse_spf(spf: str):
         results.append(r)
     return results
 
+def summarize_networks(d: dict) -> dict:
+    """
+    Take a dict containing networks as input
+    Determine if network is ipv4 or ipv6
+    Determine total count of addresses for the whole dict e.g. 
+        networks = []
+        networks.append(IPNetwork('127.0.0.1/8'))
+        size = sum([n.size for n in networks])
+    Determine total unique count of addresses for the whole dict e.g. 
+        unique_size = IPSet(networks).size
+    """
+    pass
 
 def process_domain(domain:str )-> dict:
     result = {"spf": query_spf(domain)}
@@ -155,13 +180,14 @@ def process_domain(domain:str )-> dict:
         return
     parsed_fields = parse_spf(result["spf"])
     for f in parsed_fields:
-        result[f["original"]] = {"qualifier": f["qualifier"]} 
-        r = mechanism_map[f["mechanism"]](f, domain)
+        result[str(f["original"])] = {"qualifier": f["qualifier"]} 
+        handler = mechanism_map.get(f["mechanism"], process_unknown_mechanism)
+        r = handler(f, domain)
+        result["summary"] = summarize_networks(r)
         if isinstance(r, dict):
             for k,v in r.items():
-                result[f["original"]][k] = v
-        else:
-            result[""]
+                result[f["original"]][str(k)] = v
+        
 
     return result
     
@@ -175,7 +201,7 @@ def main():
         raise SystemExit("please supply -f or -d")
 
     results = {d: process_domain(d) for d in domains}
-    print(json.dumps(results, indent=4, sort_keys = True))
+    print(json.dumps(results, indent=4))
 
 
 if __name__ == "__main__":
